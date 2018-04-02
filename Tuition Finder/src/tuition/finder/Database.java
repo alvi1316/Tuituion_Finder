3de62664ad1dff734fc21d18902f2e2d83292cce
deadhead1316 <deadhead1316@users.noreply.github.com>
2018-04-02 19:44:26 +0600

    The File Paths are Updated
    The absolute file path is updated and will work in any directory

--- Tuition Finder/src/tuition/finder/Database.java
+++ Tuition Finder/src/tuition/finder/Database.java
@@ -13,7 +13,7 @@ public class Database {
     
     public static void makeFolder(String str){
         
-        File file = new File("C:\\Users\\User\\Documents\\NetBeansProjects\\Tuition Finder\\Users\\"+str);
+        File file = new File("Users\\"+str);
         if(!file.exists()){
             file.mkdir();
         }
@@ -21,7 +21,7 @@ public class Database {
     public static void makeDb(String name,String password,String email,String username,int mobileNum,int parentNum,String age,String sex,String address,String institute,double cgpa,double ssccg,double hsccg,String description){    
         
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Documents\\NetBeansProjects\\Tuition Finder\\Users\\"+username+"\\"+username+".db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:Users\\"+username+"\\"+username+".db");
             String sql = "CREATE TABLE IF NOT EXISTS profile " + "(" 
                     + "name string,"
                     + "password string,"
@@ -49,7 +49,7 @@ public class Database {
     }
     public static void makeUsernamePassword(String username,String password){
         try{
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Documents\\NetBeansProjects\\Tuition Finder\\Users\\Usernames\\usernames.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:Users\\Usernames\\usernames.db");
             Statement stmt = conn.createStatement();
             String str = "create table if not exists usernames (username string,password string);";
             stmt.execute(str);
@@ -65,7 +65,7 @@ public class Database {
     
     public static boolean checkUsernamePassword(String username,String password) throws SQLException{
         
-        Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Documents\\NetBeansProjects\\Tuition Finder\\Users\\Usernames\\usernames.db");
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:Users\\Usernames\\usernames.db");
         Statement stmt = conn.createStatement();
         String str = String.format("SELECT password FROM usernames WHERE username = '%s'",username);
         ResultSet rs = stmt.executeQuery(str);
@@ -76,7 +76,7 @@ public class Database {
         
         String s = new String();
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Documents\\NetBeansProjects\\Tuition Finder\\Users\\Usernames\\usernames.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:Users\\Usernames\\usernames.db");
             Statement stmt = conn.createStatement();
             String str = String.format("SELECT username FROM usernames WHERE username = '%s'",username);
             ResultSet rs = stmt.executeQuery(str);
